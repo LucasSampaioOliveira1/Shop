b2642a11135484285d65cef151677a0b6388d0b2
LucasSampaioOliveira1 <lucas.sampaio1901@outlook.com>
2024-01-29 18:09:47 -0300

    first Shop

---
+++ src/app/SignIn/page.jsx
@@ -0,0 +1,11 @@
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+
+const SignIn = () => {
+    return ( 
+        <section>
+            <h1 className='text-white font-bold text-2xl p-6'> <a href="/" className='hover:text-blue-800'><ArrowBackIosIcon/> Sign In</a></h1>
+        </section>
+     );
+}
+ 
+export default SignIn;
---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+
+
+export default function Home() {
+  return (
+    <section className="text-center">
+      <h1 className="text-white font-bold text-9xl mt-52">$</h1>
+
+      <div className="mt-32 flex flex-wrap justify-center gap-7">
+        <button><a href="/SignUp" className="bg-white text-blue-500 p-4 px-10 font-bold  rounded-2xl hover:bg-blue-800 hover:transition">Sign Up</a></button>
+        <button><a href="/SignIn" className="bg-#1152FD text-white p-4 px-10 font-bold rounded-2xl  hover:bg-white hover:text-blue-500 hover:transition">Sign In</a></button>
+      </div>
+    </section>
+  );
+}
